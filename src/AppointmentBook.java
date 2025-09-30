f36c39fd0c013f88885c229d7143c63ee6886d44
Intersected2 <brianx8@nycstudents.net>
2025-09-30 15:35:06 -0400

    yes

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -9,18 +9,31 @@ public class AppointmentBook{
     public int findFreeBlock (int period, int duration){
         int block = 0;
         for (int i = 0; i < 60; i++){
-            if (isMinuteFree(period, i)){
+            if (isMinuteFree(period, i)) {
                 block++;
+                if (block == duration) {
+                    return i - duration + 1;
+                }
+            }
+            else {
+                block = 0;
             }
         }
-        return 0;
+        return -1;
     }
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        for (int i = startPeriod; i<= endPeriod; i++)
         return false;
     }
     public void printPeriod(int period){
         for(int i = 0; i < schedule[period - 1].length; i++){
            System.out.println((i + " " + schedule[period - 1][i]));
+        }
+    }
+    private void reserveBlock(int period, int startMinute, int duration){
+        for (int i = startMinute; i < startMinute + duration; i++){
+            schedule[period - 1][i] = false;
+
         }
     }
 }
