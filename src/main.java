448e6f58fcaeb2303a85e6c88f7fb94bd8664ca3
Intersected2 <brianx8@nycstudents.net>
2025-10-27 21:24:46 -0400

    yes

--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 public class main {
     public static void main (String[]args){
         boolean[][] schedule = new boolean[8][60];
@@ -8,7 +11,9 @@ public class main {
         for (int i = 5; i < 30; i++) schedule[3][i] = true;
         for (int i = 44; i < 60; i++) schedule[3][i] = true;
         a.printPeriod(2);
-        System.out.println(a.findFreeBlock(2,10));
+        System.out.println(a.findFreeBlock(2,15));
+        System.out.println(a.findFreeBlock(2,9));
+        System.out.println(a.findFreeBlock(2,20));
         AppointmentBook b = new AppointmentBook(schedule);
         int period = 2;
         while (period < 5){
@@ -18,6 +23,25 @@ public class main {
         }
         b.printPeriod(4);
         System.out.println(b.makeAppointment(2,4,22));
+        System.out.println(b.makeAppointment(3,4,3));
+        System.out.println(b.makeAppointment(2,4,30));
         b.printPeriod(4);
+    }
+    public static int read() throws FileNotFoundException {
+        int numofschedulesreserved;
+        AppointmentBook[] books = new AppointmentBook[1000];
+        File f = new File("Schedules.txt");
+        Scanner s = new Scanner(f);
+        int index = 0;
+        while (s.hasNextLine()) {
+            boolean[][] schedules = new boolean[8][60];
+            for (int i = 0; i < 60; i++){
+                boolean x = s.nextBoolean();
+                boolean print = s.nextBoolean();
+                System.out.println(print);
+
+            }
+        }
+        return 0;
     }
 }
